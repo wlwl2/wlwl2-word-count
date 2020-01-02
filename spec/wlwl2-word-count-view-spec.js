a7388ce15e04b5709614cae0fff7a2ea61d50895
William Lim <manvinedi@gmail.com>
2020-01-02 21:43:54 +0800

    first commit

---
+++ spec/wlwl2-word-count-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Wlwl2WordCountView from '../lib/wlwl2-word-count-view';
+
+describe('Wlwl2WordCountView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
